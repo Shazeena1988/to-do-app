eb98badb83cb9cdd816096b96dfff1e8feba313a
Shazeena Hussain <shazeena1988@gmail.com>
2024-03-15 12:22:37 +0000

    date stamp added

--- website/models.py
+++ website/models.py
@@ -1,6 +1,8 @@
 from . import db
+import datetime
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     task = db.Column(db.String(300), unique = True)
     complete = db.Column(db.Boolean, default = False)
+    time_created = db.Column(db.DateTime, default = datetime.date.today)
